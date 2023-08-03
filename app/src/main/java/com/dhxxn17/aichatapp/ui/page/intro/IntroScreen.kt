e8870672e3326a2a30ee23a1008c13633fca004f
xxria17 <xxrcy32@gmail.com>
2023-08-03 16:52:47 +0900

    [Update] 화면 레이아웃 수정

--- app/src/main/java/com/dhxxn17/aichatapp/ui/page/intro/IntroScreen.kt
+++ app/src/main/java/com/dhxxn17/aichatapp/ui/page/intro/IntroScreen.kt
@@ -3,6 +3,7 @@ package com.dhxxn17.aichatapp.ui.page.intro
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
@@ -14,6 +15,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
@@ -30,7 +32,8 @@ fun IntroScreen(navController: NavController) {
         modifier = Modifier
             .fillMaxSize()
             .background(Color.White),
-        horizontalAlignment = Alignment.CenterHorizontally
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
     ) {
         Text(
             text = "AI CHAT",
@@ -42,23 +45,28 @@ fun IntroScreen(navController: NavController) {
         Image(
             painter = painterResource(id = R.mipmap.ic_launcher_foreground),
             contentDescription = "",
-            modifier = Modifier.size(50.dp)
+            modifier = Modifier.size(200.dp)
         )
 
         Box(
-            modifier = Modifier.fillMaxWidth()
-                .background(
-                    brush = Brush.verticalGradient(
-                        listOf(
-                            Color(0xffF54EA2),
-                            Color(0xffFF7676)
-                        )
-                    ), shape = RoundedCornerShape(10.dp)
-                )
-                .padding(10.dp)
+            modifier = Modifier
+                .padding(horizontal = 15.dp)
+                .fillMaxWidth()
                 .clickable {
                     navController.navigate(Screens.ChatListScreen.route)
                 }
+                .background(
+                    brush = Brush.linearGradient(
+                        colors = listOf(
+                            Color(0xffFCE38A),
+                            Color(0xffF38181)
+                        ),
+                        start = Offset.Zero,
+                        end = Offset.Infinite
+                    ), shape = RoundedCornerShape(10.dp)
+                )
+                .padding(20.dp),
+            contentAlignment = Alignment.Center
         ){
             Text(
                 text = "시작하기",
--- app/src/main/java/com/dhxxn17/aichatapp/ui/page/list/ChatListScreen.kt
+++ app/src/main/java/com/dhxxn17/aichatapp/ui/page/list/ChatListScreen.kt
@@ -18,6 +18,7 @@ import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.ColorFilter
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -29,6 +30,7 @@ import com.dhxxn17.aichatapp.ui.page.Screens
 @Composable
 fun ChatListScreen(navController: NavController) {
     val viewModel : ChatListViewModel = hiltViewModel()
+    val state = viewModel.state
 
     Box(
         modifier = Modifier
@@ -36,16 +38,48 @@ fun ChatListScreen(navController: NavController) {
             .background(Color.White)
             .padding(10.dp)
     ) {
-        LazyColumn() {
-            viewModel.state.chatList.getValue(this).forEach { 
-                item {
-                    ListItem(title = it.title)
+        LazyColumn(
+            modifier = Modifier.fillMaxSize()
+        ) {
+
+            state.chatList.getValue(this).let { _chatList ->
+                if (_chatList.isEmpty()) {
+                    item {
+                        Box(
+                            modifier = Modifier.fillMaxSize()
+                                .padding(top = 350.dp),
+                            contentAlignment = Alignment.Center
+                        ) {
+                            Text(
+                                text = "새로운 대화를 생성해보세요 !",
+                                fontSize = 18.sp,
+                                color = Color.Black,
+                                fontWeight = FontWeight.ExtraBold
+                            )
+                        }
+                    }
+                } else {
+                    _chatList.forEach { _data ->
+                        item {
+                            ListItem(title = _data.title, onClick = {
+                                navController.navigate(Screens.ChatScreen.withId(
+                                    _data.id
+                                ))
+                            })
+                        }
+                    }
                 }
             }
         }
 
         Box(
             modifier = Modifier
+                .padding(end = 10.dp, bottom = 20.dp)
+                .clickable {
+                    navController.navigate(Screens.ChatScreen.withId(
+                        0L
+                    ))
+                }
                 .background(
                     brush = Brush.verticalGradient(
                         listOf(
@@ -56,10 +90,7 @@ fun ChatListScreen(navController: NavController) {
                     shape = CircleShape
                 )
                 .align(Alignment.BottomEnd)
-                .padding(end = 20.dp)
-                .clickable {
-                    navController.navigate(Screens.ChatScreen.route)
-                }
+                .padding(10.dp)
         ) {
             Image(
                 painter = painterResource(id = R.drawable.add),
@@ -73,13 +104,17 @@ fun ChatListScreen(navController: NavController) {
 
 @Composable
 private fun ListItem(
-    title: String
+    title: String,
+    onClick: () -> Unit
 ) {
     Box(
         modifier = Modifier
             .fillMaxWidth()
             .background(Color.White)
             .padding(15.dp)
+            .clickable {
+                onClick.invoke()
+            }
     ) {
         Text(
             text = title,
